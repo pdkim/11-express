58686b42d5c42e3a8013d7f5ec87ecf8f855cb88
Phillip Kim <phildkim@gmail.com>
2018-06-09 10:21:22 -0700

    start method added

--- src/app.js
+++ src/app.js
@@ -1,1 +1,30 @@
 'use strict';
+
+let http = require('http');
+
+const router = require('./lib/router.js');
+const api = require('./api/api.js');
+
+let alreadyRunning = false;
+
+const app = http.createServer(router.route);
+
+module.exports = {
+  start: (port) => {
+    if(!alreadyRunning) {
+      app.listen(port, (err) =>{
+        if(err) {throw err;}
+
+        alreadyRunning = true;
+        console.log('Server is up on port', port);
+      });
+    }
+    else {
+      console.log('Server already running on port', port);
+    }
+  },
+
+  stop: () => {
+    
+  }
+}
